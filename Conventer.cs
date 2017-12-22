7e9dcf42db0e3fa4cf204f22f7d425db749b84a0
AliaksandrDziamkovich <34609419+AliaksandrDziamkovich@users.noreply.github.com>
2017-12-22 12:57:33 +0300

    1

---
+++ Conventer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Converter
+    {
+        public List<int> ConverterTo2 (int number10)
+        {
+            List<int> x = new List<int>();
+            int number2, number;
+            for (int i = 0; i < 100000; i++)
+            {
+                if (number10 < 1)
+                {
+                    break;
+                }
+                number2 = number10 % 2;
+                number = number10 / 2;
+                number10 = number;
+
+                x.Add(i);
+                x[i] = number2;
+            }
+            return x;
+        }
+
+        public List<int> ReverseNumber (List<int> ConverterTo2)
+        {
+           ConverterTo2.Reverse();
+            return ConverterTo2;
+        }
+
+        public string NumberToString ( List<int> ReverseNumber)
+        {
+            string value = String.Concat<int>(ReverseNumber);
+            return value;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Converter converter = new Converter();
+
+            List<int> converterTo2 = converter.ConverterTo2(number);
+            List<int> reverse = converter.ReverseNumber(converterTo2);
+            string numberToString = converter.NumberToString(reverse);
+
+            Console.WriteLine(numberToString);
+            Console.ReadKey();
+        }
+    }
+}
