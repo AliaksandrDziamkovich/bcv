ff8cc3c5d28f59067e1b27d2c830db6c3e3634d8
AliaksandrDziamkovich <34609419+AliaksandrDziamkovich@users.noreply.github.com>
2017-12-29 19:22:42 +0300

    1

---
+++ ConsoleApp26/Conventer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converter
+{
+    class Converter
+    {
+        public string ConverterTo2 (int number10)
+        {
+            List<int> x = new List<int>();
+            int number;
+            while(number10>0)
+            {
+                number = number10 % 2;
+                number10 = number10/2;
+                x.Add(number);
+            }
+            x.Reverse();
+            return string.Join("", x);
+        }
+    }
+}
